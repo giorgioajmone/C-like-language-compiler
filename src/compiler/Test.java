27fa8ef550aa5016f1252a8c9a80e7a1673403cc
giorgioajmone <giorgioajmone@protonmail.com>
2022-11-25 18:51:42 +0100

    First Version

--- src/compiler/Test.java
+++ src/compiler/Test.java
@@ -8,7 +8,7 @@ import classes.*;
 
 public class Test
 {
-	public static void main( String args[] ) throws FileNotFoundException
+	public static void main(String[] args) throws FileNotFoundException
 	{		
 		SourceFile f = new SourceFile();
 		
